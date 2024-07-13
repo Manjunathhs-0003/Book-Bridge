c95b3f13c89d025c35dad41e71f18bdd4c0165d2
Manjunath HS <hsmanjunathhsmanjunath@gmail.com>
2024-07-13 20:09:22 +0530

    Added BuyBookPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider, AuthContext } from './contexts/AuthContext';
 import Layout from './components/Layout'; // Import Layout
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
 import ProfilePage from './pages/ProfilePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import BuyBookPage from './pages/BuyBookPage'; // Import BuyBookPage
 import NavBar from './components/NavBar'; // Import NavBar
 import './styles.css';
-
 
 const App = () => {
   return (
@@ -17,21 +17,33 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="*" element={
-            <Layout>
-              <NavBar /> {/* Render NavBar on all pages except HomePage */}
-              <Routes>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-              </Routes>
-            </Layout>
-          } />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="*"
+            element={
+              <Layout>
+                <NavBar /> {/* Render NavBar on all pages except HomePage */}
+                <Routes>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/profile" element={<PrivateRoute Component={ProfilePage} />} />
+                  <Route path="/buy/:id" element={<PrivateRoute Component={BuyBookPage} />} />
+                  {/* Redirect to home if no match */}
+                  <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+              </Layout>
+            }
+          />
         </Routes>
       </Router>
     </AuthProvider>
   );
+};
+
+const PrivateRoute = ({ Component }) => {
+  const { user } = useContext(AuthContext);
+
+  return user ? <Component /> : <Navigate to="/login" />;
 };
 
 export default App;
